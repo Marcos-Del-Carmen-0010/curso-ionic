72d4ccbe0744cf3b1142c799f76caefe7a5a92e3
Marcos-Del-Carmen-0010 <marcossachezdelcarmen@gmail.com>
2025-05-25 23:53:30 -0700

    componente de not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,10 @@ export const routes: Routes = [
     loadChildren: () => import('./contact/contact.module').then((m) => m.ContactModule),
   },
   {
+    path: 'not-found',
+    loadComponent: () => import('./shared/pages/not-fount/not-fount.component').then((m) => m.NotFountComponent),
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
---
+++ src/app/shared/pages/not-fount/not-fount.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-not-fount',
+  templateUrl: './not-fount.component.html',
+  styleUrls: ['./not-fount.component.scss'],
+  standalone: true,
+  imports: [
+    IonContent
+  ]
+})
+export class NotFountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+    console.log('....:::::NotFountComponent init');
+  }
+}
