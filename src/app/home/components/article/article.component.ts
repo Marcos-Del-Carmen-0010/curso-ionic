f7e80c4899559187c40818f87061b787b2e0bdd1
Marcos-Del-Carmen-0010 <marcossachezdelcarmen@gmail.com>
2025-06-06 19:50:01 -0700

    incremento y decremento de productos en la bolsa

--- src/app/home/components/article/article.component.ts
+++ src/app/home/components/article/article.component.ts
@@ -1,34 +1,95 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeModel } from 'src/app/models/Home.models';
+
+import { addIcons } from 'ionicons';
+import { add, heart, remove } from 'ionicons/icons';
 @Component({
   selector: 'app-article',
   standalone: false,
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent  implements OnInit {
+export class ArticleComponent implements OnInit {
 
   public articles: HomeModel.Articulo.IArticulo[] = [];
+  public cargando: boolean = true;
+  public cant: number;
+  public total: number;
 
-  constructor() { }
+  constructor() { 
+    addIcons({ heart, add, remove });
+    this.cant = 0;
+    this.total = 0;
+  }
 
   ngOnInit() {
     this.loadArticle();
   }
 
   loadArticle() {
-    this.articles = [
-      {
-        id: 1,
-        title: 'Angular 16',
-        description: 'Angular 16 es la última versión del framework de desarrollo web de Google.',
-        price: 100,
-        image: {
-          url: 'https://ionicframework.com/docs/img/demos/card-media.png',
-          desc: 'Logo de Angular'
+    setTimeout(() => {
+      this.articles = [
+        {
+          id: 1,
+          title: 'Pizza Margherita',
+          description: 'Clásica pizza italiana con tomate, mozzarella y albahaca fresca.',
+          price: 12,
+          image: {
+            url: 'https://ionicframework.com/docs/img/demos/card-media.png',
+            desc: 'Imagen de Pizza Margherita'
+          }
+        },
+        {
+          id: 2,
+          title: 'Hamburguesa Clásica',
+          description: 'Jugosa hamburguesa con queso, lechuga, tomate y salsa especial.',
+          price: 10,
+          image: {
+            url: 'https://ionicframework.com/docs/img/demos/card-media.png',
+            desc: 'Imagen de Hamburguesa Clásica'
+          }
+        },
+        {
+          id: 3,
+          title: 'Sushi Variado',
+          description: 'Selección de sushi fresco con salmón, atún y aguacate.',
+          price: 15,
+          image: {
+            url: 'https://ionicframework.com/docs/img/demos/card-media.png',
+            desc: 'Imagen de Sushi Variado'
+          }
+        },
+        {
+          id: 4,
+          title: 'Ensalada César',
+          description: 'Ensalada fresca con pollo, crutones, queso parmesano y aderezo César.',
+          price: 8,
+          image: {
+            url: 'https://ionicframework.com/docs/img/demos/card-media.png',
+            desc: 'Imagen de Ensalada César'
+          }
         }
-      }
-    ]
+      ];
+      this.cargando = false;
+    }, 3000);
+  }
+
+  addProduct(product: HomeModel.Articulo.IArticulo) {
+    this.cant++;
+    this.total += product.price;
+  }
+  
+  removeProduct(product: HomeModel.Articulo.IArticulo) {
+    this.cant--;
+    this.total -= product.price;
+    if (this.cant < 0) {
+      this.cant = 0;
+      this.total = 0;
+    }
+  }
+
+  validateInput(event: Event )  {
+    console.log('Evento de validación:', event.target);
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
-// import { IonicModule } from '@ionic/angular'
+import { IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput } from '@ionic/angular/standalone';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { ArticleComponent } from './components/article/article.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { IonButton, IonIcon } from '@ionic/angular/standalone';
 @NgModule({
   declarations: [
     HomeComponent,
@@ -18,12 +17,14 @@ import { SharedModule } from '../shared/shared.module';
     HomeRoutingModule,
     IonContent,
     SharedModule,
-    // IonicModule,
     IonCard, 
     IonCardContent, 
     IonCardHeader, 
     IonCardSubtitle, 
     IonCardTitle,
+    IonButton,
+    IonIcon,
+    IonInput
   ]
 })
 export class HomeModule { }
